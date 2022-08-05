e2e9a20223c4ce99f165b53c4050fa66aafee547
jyrnan <yoeking@gmail.com>
2022-08-05 15:05:41 +0800

    Initial Commit

---
+++ DemoMultiLink/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DemoMultiLink
+//
+//  Created by Yong Jin on 2022/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoMultiLink/DemoMultiLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMultiLinkApp.swift
+//  DemoMultiLink
+//
+//  Created by Yong Jin on 2022/8/5.
+//
+
+import SwiftUI
+
+@main
+struct DemoMultiLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
